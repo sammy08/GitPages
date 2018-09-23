a3452923b7afa2326796815f72b694c968e389e2
Sammy <35262710+sammy08@users.noreply.github.com>
2018-09-23 12:54:44 -0300

    Add files via upload

---
+++ Validacao.js
@@ -0,0 +1,13 @@
+
+//se o type estiver email ele só vai aceitar emails validos
+  $("#formKing").validate({
+    debug: true;
+    rules:{
+    password:{
+    minlength:1,
+    maxlength:2,
+  }
+                          
+  }
+  
+  });
